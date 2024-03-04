75471f50e3293baa3b96c5a163a899724a2958e5
Ysabel Valencia <ysavalencia@hotmail.com>
2024-03-04 23:05:04 +0100

    adding properties and constructor

--- src/YsaRating.js
+++ src/YsaRating.js
@@ -6,4 +6,19 @@ export class YsaRating extends LitElement {
       box-sizing: border-box;
     }
   `;
+
+  static properties = {
+    starsCount: { type: Number },
+    rating: { type: Number },
+    text: { type: String },
+    showTextarea: { type: Boolean },
+  };
+
+  constructor() {
+    super();
+    this.starsCount = 5;
+    this.rating = 2;
+    this.text = '¿Qué te ha parecido?';
+    this.showTextarea = false;
+  }
 }
